b30f8cbb1fda15f4893d9058390419c4f1c27ad7
Aria Li <Qian.Li@walmartlabs.com>
2020-12-29 17:23:48 -0800

    add

--- Image/load_url.py
+++ Image/load_url.py
@@ -1,4 +1,7 @@
+import os
 import time
+from PIL import Image
+from io import BytesIO
 import numpy as np
 import logging
 import asyncio
@@ -13,20 +16,31 @@ def sleep_between_retry(url):
     time.sleep(x)
 
 
-async def fetch(row, session):
+async def fetch(row, session, url_col, parent_directory=None):
 
     HTTP_STATUS_CODES_TO_RETRY = [500, 502, 503, 504]
     num_retries = 2
-    url = row['url']
+    url = row[url_col]
+    
+    # check if the image has been downloaded
+    img_path = os.path.join(parent_directory, row['img_name'])
+    if os.path.exists(img_path):
+        return row, None
 
     for x in range(num_retries):
         error_message = ''
         try:
             # async with timeout(5):
-            async with session.get(url, timeout=20) as response: # can get proxy in get
+            async with session.get(url, timeout=20) as response: # proxy is optional for get
                 resp = await response.read()
-                im_array = np.asarray(bytearray(resp), dtype=np.uint8)
-                return row, im_array
+                im = Image.open(BytesIO(resp))
+                if im.mode != 'RGB':
+                    im = im.convert('RGB')
+                im.save(img_path)
+                
+                # if not to save, but load numpy.array, use:
+                #im_array = np.asarray(bytearray(resp), dtype=np.uint8)
+                return row, im
 
         except ClientResponseError as e:
             error_message = 'Image download failed for {}. Received bad status {}'.format(url, e)
@@ -55,19 +69,18 @@ async def fetch(row, session):
     return row, None
 
 
-async def fetch_all(input_dict, is_BytesIO=True):
+async def fetch_all(input_dict, url_col, parent_directory=None):
     """Launch requests for all web pages."""
     tasks = []
     image_array = list()
     file_list = list()
     async with ClientSession(raise_for_status=True, connector=TCPConnector(verify_ssl=False)) as session:
         for row in input_dict:
-            task = asyncio.ensure_future(fetch(row, session)) # row, session
+            task = asyncio.ensure_future(fetch(row, session, url_col, parent_directory))
             tasks.append(task) # create list of tasks
         for response in await asyncio.gather(*tasks): # gather task responses
             image_array.append(response[1])
             file_list.append(response[0])
     logging.warning('the {} batch succeeded'.format(len(input_dict)))
     return file_list, image_array
-
 
